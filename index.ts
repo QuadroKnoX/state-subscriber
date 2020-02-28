59d55d627c4532d6977d320c9fcada4cd2130c50
Oliver Freudrich <info@oliverfreudrich.com>
2020-02-28 19:11:12 +0100

    updated types

--- index.ts
+++ index.ts
@@ -9,26 +9,26 @@ export default class StateSubscriber extends EventEmitter2 {
     super();
   }
 
-  next(event: string, data: any) {
+  next(event: string, data: any): boolean {
     this._recentValues[event] = data;
     return this.emit(event, data);
   }
 
-  subscribe(event: string, cb: (data: any) => void) {
+  subscribe(event: string, cb: (data: any) => void): EventEmitter2 {
     this.addListener(event, cb);
     cb(this._recentValues[event]);
 
     return this;
   }
 
-  subscribeOnce(event, cb: (data: any) => void) {
+  subscribeOnce(event: string, cb: (data: any) => void): EventEmitter2 {
     this.once(event, cb);
     cb(this._recentValues[event]);
 
     return this;
   }
 
-  unsubscribe(event, cb: (data: any) => void) {
+  unsubscribe(event: string, cb: (data: any) => void): EventEmitter2 {
     this.removeListener(event, cb);
     return this;
   }
