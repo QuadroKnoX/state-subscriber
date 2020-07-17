9a8ed45569505681c1e6d0902abf4e16aa5f7245
Oliver Freudrich <info@oliverfreudrich.com>
2020-07-17 14:57:40 +0200

    * added documentation + added option to only get updates instead of all + updated packages

--- index.js
+++ index.js
@@ -1,29 +1,63 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-const eventemitter2_1 = require("eventemitter2");
+'use strict';
+Object.defineProperty(exports, '__esModule', { value: true });
+const eventemitter2_1 = require('eventemitter2');
 class StateSubscriber extends eventemitter2_1.EventEmitter2 {
     constructor() {
         super();
+        /*
+         * Set this to true, if you want only receive updates on next events.
+         */
+        this.updatesOnly = false;
+        /*
+         * The internal cache to keep the last values of an event.
+         */
         this._recentValues = {};
     }
+    /*
+     * Next will update the state of the last value of an event.
+     * If you set updatesOnly to true, then it will prevent from sending duplicate event values.
+     */
     next(event, data) {
+        if (
+            this.updatesOnly &&
+            this._recentValues[event] &&
+            this._recentValues[event] === data
+        ) {
+            return false;
+        }
         this._recentValues[event] = data;
         return this.emit(event, data);
     }
+    /*
+     * Subscribe will receive the values from next or emit.
+     * Similar to .on() but you will receive the last value on subscription from the next event.
+     */
     subscribe(event, cb) {
         this.addListener(event, cb);
         cb(this._recentValues[event]);
         return this;
     }
+    /*
+     * Subscribe will receive the last value from next.
+     * Similar to .once() but you will receive the last value on subscription from the next event once.
+     */
     subscribeOnce(event, cb) {
         this.once(event, cb);
         cb(this._recentValues[event]);
         return this;
     }
+    /*
+     * Unsubscribe will unsubscribe from an event.
+     * Similar to .removeListener().
+     */
     unsubscribe(event, cb) {
         this.removeListener(event, cb);
         return this;
     }
+    /*
+     * UnsubscribeAll will unsubscribe all listeners from an event.
+     * Similar to .removeAllListeners().
+     */
     unsubscribeAll(event) {
         this.removeAllListeners(event);
         return this;
--- index.ts
+++ index.ts
@@ -1,40 +1,75 @@
 import { EventEmitter2 } from 'eventemitter2';
 
 export default class StateSubscriber extends EventEmitter2 {
-  _recentValues: {
-    [event: string]: any;
-  } = {};
+    /*
+     * Set this to true, if you want only receive updates on next events.
+     */
+    updatesOnly: boolean = false;
+    /*
+     * The internal cache to keep the last values of an event.
+     */
+    _recentValues: {
+        [event: string]: any;
+    } = {};
 
-  constructor() {
-    super();
-  }
+    constructor() {
+        super();
+    }
 
-  next(event: string, data: any): boolean {
-    this._recentValues[event] = data;
-    return this.emit(event, data);
-  }
+    /*
+     * Next will update the state of the last value of an event.
+     * If you set updatesOnly to true, then it will prevent from sending duplicate event values.
+     */
+    next(event: string, data: any): boolean {
+        if (
+            this.updatesOnly &&
+            this._recentValues[event] &&
+            this._recentValues[event] === data
+        ) {
+            return false;
+        }
 
-  subscribe(event: string, cb: (data: any) => void): EventEmitter2 {
-    this.addListener(event, cb);
-    cb(this._recentValues[event]);
+        this._recentValues[event] = data;
+        return this.emit(event, data);
+    }
 
-    return this;
-  }
+    /*
+     * Subscribe will receive the values from next or emit.
+     * Similar to .on() but you will receive the last value on subscription from the next event.
+     */
+    subscribe(event: string, cb: (data: any) => void): EventEmitter2 {
+        this.addListener(event, cb);
+        cb(this._recentValues[event]);
 
-  subscribeOnce(event: string, cb: (data: any) => void): EventEmitter2 {
-    this.once(event, cb);
-    cb(this._recentValues[event]);
+        return this;
+    }
 
-    return this;
-  }
+    /*
+     * Subscribe will receive the last value from next.
+     * Similar to .once() but you will receive the last value on subscription from the next event once.
+     */
+    subscribeOnce(event: string, cb: (data: any) => void): EventEmitter2 {
+        this.once(event, cb);
+        cb(this._recentValues[event]);
 
-  unsubscribe(event: string, cb: (data: any) => void): EventEmitter2 {
-    this.removeListener(event, cb);
-    return this;
-  }
+        return this;
+    }
 
-  unsubscribeAll(event: string): EventEmitter2 {
-    this.removeAllListeners(event);
-    return this;
-  }
+    /*
+     * Unsubscribe will unsubscribe from an event.
+     * Similar to .removeListener().
+     */
+    unsubscribe(event: string, cb: (data: any) => void): EventEmitter2 {
+        this.removeListener(event, cb);
+        return this;
+    }
+
+    /*
+     * UnsubscribeAll will unsubscribe all listeners from an event.
+     * Similar to .removeAllListeners().
+     */
+    unsubscribeAll(event: string): EventEmitter2 {
+        this.removeAllListeners(event);
+        return this;
+    }
 }
